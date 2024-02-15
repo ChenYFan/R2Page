5cd983b190492ca89d71ac8f3b63943c86cebd0a
CyanFalse <chen2778754364@foxmail.com>
2024-02-15 09:36:50 +0800

    test

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ import fs from 'fs';
 if (!fs.existsSync('dist')) fs.mkdirSync('dist');
 //创建dist/1目录
 if (!fs.existsSync('dist/1')) fs.mkdirSync('dist/1');
-//下载文件到dist/1目录
-request('https://sgp-ping.vultr.com/vultr.com.1000MB.bin').pipe(fs.createWriteStream('dist/1/1.dd'));
+//生成5个10M的文件
+for (let i = 0; i < 5; i++) {
+    fs.writeFileSync(`dist/1/${i}.txt`, new Buffer(1024 * 1024 * 10));
+}
